65ad9f1a324f21561eec40fa8f76d0e4506fe85e
parupupa <kanaitourokuyou@yahoo.co.jp>
2026-04-29 19:06:18 +0900

    Recipeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  has_many :recipes, dependent: :destroy
 end
