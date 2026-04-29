7be680b2d2b43b64e019358fe9553e06e0fa2b13
parupupa <kanaitourokuyou@yahoo.co.jp>
2026-04-29 19:29:56 +0900

    RecipeVersionモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_29_100127) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_29_101043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipe_versions", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.string "version_name"
+    t.integer "version_number"
+    t.text "ingredients"
+    t.text "steps"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_recipe_versions_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -35,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_29_100127) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipe_versions", "recipes"
   add_foreign_key "recipes", "users"
 end
