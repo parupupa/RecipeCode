2a3021dcf9da3ac3201d3ba371d36cb53f5f47f6
parupupa <kanaitourokuyou@yahoo.co.jp>
2026-04-29 13:47:42 +0900

    ユーザー登録機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   def after_sign_in_path_for(resource)
     root_path
   end
