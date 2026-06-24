ba1027b1915799d2244cd00966dc1b203b800d2d
parupupa <kanaitourokuyou@yahoo.co.jp>
2026-06-25 07:09:45 +0900

    パスワードリセットメールConfig設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {
+    host: 'recipecode.onrender.com',
+    protocol: 'https'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['noreply.recipecode@gmail.com'],
+    password: ENV['mnggevteqnyxbhko'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
